77ee710ca1e83a2cdf809a7a3a46f01dbd059ef8
nameczz <zizhao.chen@zilliz.com>
2021-04-27 11:20:43 +0800

    add ci

---
+++ trigger-milvus-build.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const token = process.env.GH_TOKEN;
+const prodRes = await axios.post(
+  "https://api.github.com/repos/milvus-io/www.milvus.io/dispatches",
+  { event_type: "prod-deploy" },
+  {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }
+);
+console.log("---- prod deploy -----", prodRes);
+
+const devRes = await axios.post(
+  "https://api.github.com/repos/milvus-io/www.milvus.io/dispatches",
+  { event_type: "dev-deploy" },
+  {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }
+);
+console.log("---- dev deploy -----", devRes);
